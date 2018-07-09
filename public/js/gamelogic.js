bc67109242b489290869eac94ca3ab9635c643e7
unknown <sam1992.123@gmail.com>
2018-07-09 08:05:59 +0530

    buttons added instead of divs for user input

--- public/js/gamelogic.js
+++ public/js/gamelogic.js
@@ -2,7 +2,7 @@ $(function () {
   
     var currentPlayer = 1; 
     var arrP1 = [];
-    var arrP1 = [];
+    var arrP2 = [];
 
    var $btnG = $(".btnGrid");
 
@@ -10,13 +10,15 @@ $(function () {
         console.log($btnG);
         
         if (currentPlayer===1) {
-            //arrP1.push(this.val());
+            arrP1.push(+$(this).val());
             currentPlayer = 2;
             this.innerText = 'X';
+            console.log(arrP1); 
         } else {
-            //arrP2.push(this.val());
+            arrP2.push(+$(this).val());
             currentPlayer = 1;
             this.innerText='O';
+            console.log(arrP2);
         }
     });
 
