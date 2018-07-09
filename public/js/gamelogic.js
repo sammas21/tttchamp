b92b91fa00a45480a52ce3746f58d126813f89cd
Sammas21 <sam1992.123@gmail.com>
2018-07-09 19:17:43 +0530

    UI improved of play grid

--- public/js/gamelogic.js
+++ public/js/gamelogic.js
@@ -5,22 +5,26 @@ $(function () {
     var arrP2 = [];
 
    var $btnG = $(".btnGrid");
-
-    $btnG.click(function(){
+   var $instMsg = $('#inst');
+   $instMsg.html('Player 1 your turn');
+   
+   $btnG.click(function(){
         console.log($btnG);
         
         if (currentPlayer===1) {
             arrP1.push(+$(this).val());
             currentPlayer = 2;
             this.innerText = 'X';
-            console.log(arrP1); 
+            console.log(arrP1);
+            $instMsg.html('Player 2 your turn'); 
         } else {
             arrP2.push(+$(this).val());
             currentPlayer = 1;
             this.innerText='O';
             console.log(arrP2);
+            $instMsg.html('Player 1 your turn');
         }
+        $(this).prop('disabled', true);
+
     });
-
-
 });
